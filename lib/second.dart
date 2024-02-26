da11e4af2a6435dd68ba2721432377551e57e1ca
alexey <a.ustinnikov@grfc.ru>
2024-02-26 22:46:27 +0300

    successful request to the course details on tap

--- lib/second.dart
+++ lib/second.dart
@@ -11,18 +11,18 @@ class SecondRoute extends StatelessWidget {
 
   Future<String> getCourseDetails(String slug) async {
     const String baseUrl = 'api.coursera.org';
-    const String charactersPath = '/api/onDemandCourseMaterials.v1';
+    const String charactersPath = '/api/onDemandCourses.v1';
     final Map<String, String> queryParameters = <String, String>{
       'q': 'slug',
       'slug': slug,
     };
     final uri = Uri.https(baseUrl, charactersPath, queryParameters);
-    var response = await http.get(uri, headers: {
-      'Cookie':
-          'CAUTH=iSnxUuUXexAVmDYebtjE896f7sAJW2qPFR2mmFvI6nmnGWRnuqyu2suTtABxT3xDxkwW5ZNhuLEk4SxmNgKBtQ.uHY97RxCQc62nmCxUP1jhg.vO5Z3SL-Zw-EXvJSD_wPd8Q8muyj9xXJIbhKg-lDxeUp4vKqIkFarZikHun2EPFfrYOlHbkAgIXOQkEONYd50wfj_O4ijoCBfxu6A_yRfU_9UGZLKucTyXS-cGx_8Nk246ZxYns0OLlDXOL0cePVhEeLi9FjdFXNg8JWngJLuqj3GhR5BpmwxKq8nHeo5GFZCqIgI-9izlJbNcqPj12mb1Q8fjPWWtZy8Q16P8wAGMUpeqOcRtZ0xolZqPE6onNd6Ay38adR3-ZsarG76DYqrrkwPlWiTH4R4Ukb3Sf0X_Ip_42QS8AfRFEQW9_NdCjjJZz8EfnecKsGTYb7OrmrP37dvo24Kvk72H0NqDhcj8U4d-lTeG3NNBC3H8KaWrGy1WaPVa7thNkTvfymxsZFPt9S7cIUC2dYQ5NI710ku6w'
-    });
-    // print('Response statusCode from getListOfCourses: ${response.statusCode}');
-    // print('Response body from getListOfCourses: ${response.body}');
+    var response =
+        await http.get(uri, headers: {'Cookie': 'CAUTH=${main.cauth}'});
+
+    print('slug is $slug');
+    print('Response statusCode from getCourseDetails: ${response.statusCode}');
+    print('Response body from getCourseDetails: ${response.body}');
 
     if (response.statusCode == 200) {
       var jsonResponse =
@@ -36,8 +36,6 @@ class SecondRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('test1');
-    print(main.cauth);
     return MaterialApp(
       home: DefaultTabController(
         length: 2,
@@ -62,8 +60,19 @@ class SecondRoute extends StatelessWidget {
                   title: Text(items.first),
                 ),
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(items[index]),
+                  return GestureDetector(
+                    // When the child is tapped, show a snackbar.
+                    onTap: () async {
+                      var desc = await getCourseDetails(items[index]);
+                      var snackBar = SnackBar(content: Text(desc));
+
+                      if (!context.mounted) return;
+
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: ListTile(
+                      title: Text(items[index]),
+                    ),
                   );
                 },
               ),
